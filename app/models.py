f5ea545ca221cf86a0ca41c0db2861fbc5b843a8
Aarushi151 <aarushi262201@gmail.com>
2026-03-31 22:40:59 +0530

    Update models.py

--- app/models.py
+++ app/models.py
@@ -9,3 +9,20 @@ class User(Base):
     email = Column(String, unique=True, index=True, nullable=False)
     password = Column(String, nullable=False)
     role = Column(String, default="job_seeker")  # recruiter / job_seeker
+class Job(Base):
+    __tablename__ = "jobs"
+
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String)
+    description = Column(String)
+    location = Column(String)
+    skills = Column(String)
+    recruiter_id = Column(Integer)
+
+class Application(Base):
+    __tablename__ = "applications"
+
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer)
+    job_id = Column(Integer, ForeignKey("jobs.id"))
+    resume_link = Column(String)
